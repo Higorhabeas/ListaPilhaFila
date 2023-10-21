72257b1c176ae34f49e158eb8348dfc96da67933
Higor <higoras.silva0@gmail.com>
2023-10-21 09:08:21 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program{
+    static void Main(String[] args){
+        int [] vetor = {3,6,1,2,3,6,4,2,3,1};
+        PrintVetor(vetor);
+    }
+
+    public static void PrintVetor(int[] vetorPrint){
+        foreach( var num in vetorPrint){
+            Console.Write(num + " ");
+            Console.WriteLine();
+        }
+    }
+}
