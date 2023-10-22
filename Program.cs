67a547138899971f84fd2cc5060ef3480b6494ec
Higor <higoras.silva0@gmail.com>
2023-10-22 18:52:32 -0300

    vesão de lista para entregar

--- Program.cs
+++ Program.cs
@@ -1,5 +1,149 @@
 ﻿using System;
 
+class StaticLista{
+    private int[] Lista;
+    private int tamanhoMax;
+    private int indArray;
+
+    public StaticLista(int tamanho){
+
+        tamanhoMax = tamanho;
+        Lista = new int[tamanho];
+        indArray = -1;
+    }
+
+    public bool EstaVazia(){
+
+        return indArray == -1;
+    }
+
+    public bool EstaCheia(){
+        
+        return indArray == tamanhoMax -1;
+    }
+
+    public int LerElemento(int j){
+
+        if(EstaVazia())
+        {
+            Console.WriteLine("Lista está vazia. Não é possível ler elementos.");
+            return -1; // Valor padrão para indicar erro             
+        }
+       
+        if(j >= 1 && j <= tamanhoMax)
+        {
+
+            return Lista[j];
+        }
+        else
+        {
+
+            Console.WriteLine("ERRO: elemento não existe.");
+            return -1; // Valor padrão para indicar erro
+        }
+
+       
+    }
+
+    public int LocalizarElemento(int elemento){
+
+        if(EstaVazia()){
+            Console.WriteLine("Lista está vazia. Não é possível ler elementos.");
+            return -1; // Valor padrão para indicar erro    
+        }
+        int i = 0;
+        int posicaoElemento = -1;
+        bool achou = false;
+        while (!achou && i < tamanhoMax){
+            
+            if(Lista[i] == elemento){
+                achou = true;
+                posicaoElemento = i;
+            }
+            i++;
+        }
+
+        if(!achou){
+            Console.WriteLine("Elemento não encontrado.");
+            return -1; // Valor padrão para indicar erro    
+        }
+
+        return posicaoElemento;
+    }
+
+    public int RemoverElemento(int posElemento){
+        if(EstaVazia()){
+            Console.WriteLine("Lista está vazia. Não é possível remover elementos.");
+            return -1; // Valor padrão para indicar erro  
+        }
+        int elementoRemovido = 0;
+        if(posElemento >= 1 && posElemento <= tamanhoMax){
+            elementoRemovido = Lista[posElemento-1];
+            for (int i = posElemento; i < tamanhoMax - 1; i++){
+                Lista[i] = Lista[i-1];
+            }
+        }else{
+            Console.WriteLine("Elemento não existe.");
+            return -1; // Valor padrão para indicar erro  
+        }
+
+        return elementoRemovido;
+    }
+
+    public void InserirElemento(int elemento){
+        if(EstaCheia()){
+            Console.WriteLine("Lista está cheia. Não é possível inserir elementos.");
+            return ; 
+        }
+        bool achouElementoVazio = false;
+        int i = 0;
+        while (!achouElementoVazio && i < tamanhoMax)
+        {
+            if( Lista[i] == 0 ){
+                Lista[i] = elemento;
+                indArray = i;
+                Console.WriteLine("Elemento inserido: " + Lista[i] + " Posição: " + i);
+                achouElementoVazio = true;
+            }
+            i++;
+        }
+    }
+
+    public  void ConcatenaListas(int[] Lista1, int[] Lista2){
+        int posicaoVazia1 = -1;
+        int posicaoVazia2 = -1;
+        
+        for ( int i = 0; i < Lista1.Length; i++){
+            if(Lista1[i] == 0){
+                posicaoVazia1 = i;
+            }
+        }
+
+        for (int j = 0; j < Lista2.Length; j++){
+            if(Lista2[j] ==0){
+                posicaoVazia2 = j;
+            }
+        }
+        int[] vetorResultante = new int[Lista1.Length + Lista2.Length];
+
+        for (int m = 0; m < posicaoVazia1; m++ ){
+
+            vetorResultante[m] = Lista1[m];
+        }
+
+        for(int n = 0; n < posicaoVazia2; n++){
+
+            vetorResultante[posicaoVazia1] = Lista2[n];
+            posicaoVazia1 ++;
+        }
+
+        for (int r = 0; r < vetorResultante.Length; r++){
+            Console.Write(" " + vetorResultante[r]);
+        }
+
+    }
+}
+
 class StaticPilha {
     private int[] Pilha;
     private int tamanhoMax;
@@ -211,5 +355,58 @@ class Program
         }
         
         Console.WriteLine("Pilha está vazia? " + Pilha.EstaVazia());
+
+        /*criando a Lista com 5 elementos*/
+        StaticLista Lista = new(5);
+         /*inserindo  elementos na lista*/
+        Lista.InserirElemento(10);
+        
+        Lista.InserirElemento(20);
+        
+        Lista.InserirElemento(30);
+
+        /*verificando e consultando a posição do elemento na lista*/
+        Console.WriteLine("A posição do elemento é: " + Lista.LocalizarElemento(20));
+
+        Lista.InserirElemento(40);
+
+        Lista.RemoverElemento(Lista.LocalizarElemento(20));
+
+        /*concatenando listas*/
+
+        StaticLista Lista1 = new(5);
+        Lista1.InserirElemento(10);
+        
+        Lista1.InserirElemento(20);
+        
+        Lista1.InserirElemento(30);
+
+        StaticLista Lista2 = new(5);
+
+        Lista2.InserirElemento(11);
+        
+        Lista2.InserirElemento(22);
+        
+        Lista2.InserirElemento(33);
+
+        int[] vetorLista1 = new int[5];
+
+        for (int p = 0; p < vetorLista1.Length; p++){
+
+            vetorLista1[p] = Lista1.LerElemento(p);
+        }
+
+        int[] vetorLista2 = new int[5];
+
+        for (int q = 0; q < vetorLista2.Length; q++){
+
+            vetorLista2[q] = Lista2.LerElemento(q);
+        }
+
+        Lista.ConcatenaListas(vetorLista1,vetorLista2);
+
+
+
+        
     }
 }
