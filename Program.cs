4215a64181b043f9edb222758482b6df77255c97
Higor <higoras.silva0@gmail.com>
2023-10-26 20:15:29 -0300

    Refatorando para entrar o mesmo

--- Program.cs
+++ Program.cs
@@ -204,7 +204,7 @@ class StaticFila
         /*declarando o que iremos precisar para FILA
         Fila como array; Comprimento de Fila; Primeira e Ultima posição*/
     private int[] Fila;
-    private int tamanhoMax;
+    public int tamanhoMax;
     private int primeiroFila;
     private int ultimoFila;
 
@@ -293,12 +293,16 @@ class Program
     static void Main(string[] args)
     {
         /*criando a fila com 5 elementos*/
+        
+
         StaticFila Fila = new(5);
 
         /*inserindo  elementos na fila*/
-        Fila.Enfileirar(1);
-        Fila.Enfileirar(2);
-        Fila.Enfileirar(3);
+        Fila.Enfileirar(10);
+        Fila.Enfileirar(20);
+        Fila.Enfileirar(30);
+
+        
 
         /*verificando e consultando o primeiro elemento da fila*/
         Console.WriteLine("Elemento da frente: " + Fila.Verificar());
@@ -309,9 +313,9 @@ class Program
 
         Console.WriteLine("Elemento da frente após a remoção: " + Fila.Verificar());
 
-        Fila.Enfileirar(4);
-        Fila.Enfileirar(5);
-        Fila.Enfileirar(6);
+        Fila.Enfileirar(40);
+        Fila.Enfileirar(50);
+        Fila.Enfileirar(60);
         /*testando overflow*/
         Fila.Enfileirar(7);
 
@@ -324,86 +328,86 @@ class Program
         }
 
         Console.WriteLine("Fila está vazia? " + Fila.EstaVazia());
-
+    
         /*TRABALHANDO COM PILHA*/
         /*criando uma pilha com 5 elementos*/
-        StaticPilha Pilha = new(5);
+        //StaticPilha Pilha = new(5);
          /*inserindo  elementos na pilha*/
-        Pilha.Empilha(1);
-        Pilha.Empilha(2);
-        Pilha.Empilha(3);
-        Pilha.Empilha(4);
+        //Pilha.Empilha(1);
+        //Pilha.Empilha(2);
+        //Pilha.Empilha(3);
+        //Pilha.Empilha(4);
 
         /*verificando e consultando o topo da pilha*/
-        Console.WriteLine("Elemento do topo: " + Pilha.ConsultaTopo());
+        //Console.WriteLine("Elemento do topo: " + Pilha.ConsultaTopo());
 
         /*removendo o elemento da pilha e mostrando qual elemento foi retirado*/
-        int elementoRemovido = Pilha.Desempilha();
-        Console.WriteLine("Elemento removido da pilha: " + elementoRemovido);
+        //int elementoRemovido = Pilha.Desempilha();
+       // Console.WriteLine("Elemento removido da pilha: " + elementoRemovido);
 
-        Console.WriteLine("Elemento da frente após a remoção da pilha: " + Pilha.ConsultaTopo());
+        //Console.WriteLine("Elemento da frente após a remoção da pilha: " + Pilha.ConsultaTopo());
 
-        Pilha.Empilha(5);
-        Pilha.Empilha(6);
+        //Pilha.Empilha(5);
+        //Pilha.Empilha(6);
         /*testando overflow*/
-        Pilha.Empilha(7);
+        //Pilha.Empilha(7);
 
         /*removendo elementos da pilha enquanto não estiver vazia*/
-        while (!Pilha.EstaVazia()){
+        //while (!Pilha.EstaVazia()){
 
-            Console.WriteLine("Elemento removido da pilha: "+ Pilha.Desempilha());
-        }
+            //Console.WriteLine("Elemento removido da pilha: "+ Pilha.Desempilha());
+       // }
         
-        Console.WriteLine("Pilha está vazia? " + Pilha.EstaVazia());
+        //Console.WriteLine("Pilha está vazia? " + Pilha.EstaVazia());
 
         /*criando a Lista com 5 elementos*/
-        StaticLista Lista = new(5);
+        //StaticLista Lista = new(5);
          /*inserindo  elementos na lista*/
-        Lista.InserirElemento(10);
+        //Lista.InserirElemento(10);
         
-        Lista.InserirElemento(20);
+        //Lista.InserirElemento(20);
         
-        Lista.InserirElemento(30);
+        //Lista.InserirElemento(30);
 
         /*verificando e consultando a posição do elemento na lista*/
-        Console.WriteLine("A posição do elemento é: " + Lista.LocalizarElemento(20));
+        //Console.WriteLine("A posição do elemento é: " + Lista.LocalizarElemento(20));
 
-        Lista.InserirElemento(40);
+        //Lista.InserirElemento(40);
 
-        Lista.RemoverElemento(Lista.LocalizarElemento(20));
+        //Lista.RemoverElemento(Lista.LocalizarElemento(20));
 
         /*concatenando listas*/
 
-        StaticLista Lista1 = new(5);
-        Lista1.InserirElemento(10);
+        //StaticLista Lista1 = new(5);
+        //Lista1.InserirElemento(10);
         
-        Lista1.InserirElemento(20);
+        //Lista1.InserirElemento(20);
         
-        Lista1.InserirElemento(30);
+        //Lista1.InserirElemento(30);
 
-        StaticLista Lista2 = new(5);
+        //StaticLista Lista2 = new(5);
 
-        Lista2.InserirElemento(11);
+        //Lista2.InserirElemento(11);
         
-        Lista2.InserirElemento(22);
+        //Lista2.InserirElemento(22);
         
-        Lista2.InserirElemento(33);
+        //Lista2.InserirElemento(33);
 
-        int[] vetorLista1 = new int[5];
+        //int[] vetorLista1 = new int[5];
 
-        for (int p = 0; p < vetorLista1.Length; p++){
+        //for (int p = 0; p < vetorLista1.Length; p++){
 
-            vetorLista1[p] = Lista1.LerElemento(p);
-        }
+            //vetorLista1[p] = Lista1.LerElemento(p);
+        //}
 
-        int[] vetorLista2 = new int[5];
+        //int[] vetorLista2 = new int[5];
 
-        for (int q = 0; q < vetorLista2.Length; q++){
+        //for (int q = 0; q < vetorLista2.Length; q++){
 
-            vetorLista2[q] = Lista2.LerElemento(q);
-        }
+            //vetorLista2[q] = Lista2.LerElemento(q);
+        //}
 
-        Lista.ConcatenaListas(vetorLista1,vetorLista2);
+        //Lista.ConcatenaListas(vetorLista1,vetorLista2); */
 
 
 
